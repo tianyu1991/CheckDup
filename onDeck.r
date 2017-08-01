eaeba2efe8e1213d69c1411b53bfe0271dfb4590
tianyu liu <hulan1991@gmail.com>
2017-08-01 10:31:52 -0400

    Update onDeck.r

--- onDeck.r
+++ onDeck.r
@@ -6,19 +6,34 @@ data$AgeOld<-cut(as.numeric(data$days_delinquent_old),c(0,1,5,10,30,60,200),righ
 summary(data$days_delinquent_new)
 data$AgeNew<-cut(as.numeric(data$days_delinquent_new),c(0,1,5,10,30,60,200),right=FALSE,labels=c(1:6))
 
+test<-data[which(data$AgeOld=='2'),]
+library(plyr)
+y = count(test, 'AgeNew')
+y$p<-y$freq/nrow(test)
+
+test2<-data[which(data$AgeOld=='3'),]
+y2 = count(test2, 'AgeNew')
+y2$p<-y2$freq/nrow(test)
+
+test3<-merge(y,y2,by="AgeNew")
+
 for( i in 1:6){
 	subdata<-data[which(data$AgeOld==i),]
-	sum = count(subdata, 'AgeNew')
+	r = count(subdata, 'AgeNew')
 	y<-paste("From",i,sep="")
-	sum[,y]<-sum$freq/nrow(subdata)
-	sum<-sum[,c(1,3)]
+	r[,y]<-r$freq/nrow(subdata)
+	r<-r[,c(1,3)]
 	
 	if (i==1){
-		result<-sum
+		result<-r
 	}
 	else{
-		result<-merge(result,sum,by="AgeNew",all=TRUE)
+		result<-merge(result,r,by="AgeNew",all=TRUE)
 	}
 }
 
 result[is.na(result)] <- 0
+
+aggregate(new_outstanding_principal_balance ~ AgeNew, subdata,FUN=sum)
+
+
