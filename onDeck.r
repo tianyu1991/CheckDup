da786a95c95df0a0df7e5279ecd18bb68de48f2a
tianyu liu <hulan1991@gmail.com>
2017-08-01 10:10:36 -0400

    Update onDeck.r

--- onDeck.r
+++ onDeck.r
@@ -6,3 +6,19 @@ data$AgeOld<-cut(as.numeric(data$days_delinquent_old),c(0,1,5,10,30,60,200),righ
 summary(data$days_delinquent_new)
 data$AgeNew<-cut(as.numeric(data$days_delinquent_new),c(0,1,5,10,30,60,200),right=FALSE,labels=c(1:6))
 
+for( i in 1:6){
+	subdata<-data[which(data$AgeOld==i),]
+	sum = count(subdata, 'AgeNew')
+	y<-paste("From",i,sep="")
+	sum[,y]<-sum$freq/nrow(subdata)
+	sum<-sum[,c(1,3)]
+	
+	if (i==1){
+		result<-sum
+	}
+	else{
+		result<-merge(result,sum,by="AgeNew",all=TRUE)
+	}
+}
+
+result[is.na(result)] <- 0
