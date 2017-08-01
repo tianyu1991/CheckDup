605140cc8ed611258c61194209e3f7fa60faa7f4
tianyu liu <hulan1991@gmail.com>
2017-08-01 09:34:03 -0400

    Update onDeck.r

--- onDeck.r
+++ onDeck.r
@@ -1,3 +1,5 @@
+data<-read.csv("OnDeck Analytics Asssignment.csv")
+
 summary(data$days_delinquent_old)
 data$AgeOld<-cut(as.numeric(data$days_delinquent_old),c(0,1,5,10,30,60,200),right=FALSE,labels=c(1:6))
 
